b6e1bf7448e988df1849e25f5769fab73952cbad
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 02:07:44 +0100

    create laplateforme.sql file that contains the complete backup of my database

---
+++ Jour01/job07.sql
@@ -0,0 +1,11 @@
+# Create a laplateforme.sql file in the current directory
+# This file laplateforme.sql contains the complete backup of my database
+# This file can be shared on GitHub or used to restore my database elsewhere.
+
+rs_5@MacBook-Pro-de-Rayanne Jour01 % mysqldump -u root -p LaPlateforme > laplateforme.sql
+
+Enter password: 
+rs_5@MacBook-Pro Jour01 % ls
+job01.sql               job04.sql               job07.sql
+job02.sql               job05.sql               laplateforme.sql
+job03.sql               job06.sql
