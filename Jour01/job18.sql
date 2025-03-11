a2ad07ef617fd279c92ac77e8ec9dac210c838c9
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 03:15:10 +0100

    delete a student

---
+++ Jour01/job18.sql
@@ -0,0 +1,8 @@
+mysql> DELETE FROM etudiant WHERE nom = "Doe" AND prenom = "John";
+Query OK, 1 row affected (0.00 sec)
+
+mysql> SELECT * FROM etudiant WHERE nom = "Doe" AND prenom = "John";
+Empty set (0.00 sec)
+
+mysql> 
+
