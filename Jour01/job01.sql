75dd1d9c30c8a5e4b4239a7447a654c6da59094e
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 01:17:49 +0100

    launching MySQL server

---
+++ Jour01/job01.sql
@@ -0,0 +1,13 @@
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 10
+Server version: 9.2.0 Homebrew
+
+Copyright (c) 2000, 2025, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> 
