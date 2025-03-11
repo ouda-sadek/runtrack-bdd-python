2de9120490fb2f975c85a323129d944e741cf509
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 03:10:18 +0100

    change the value of Betty's age

---
+++ Jour01/job17.sql
@@ -0,0 +1,14 @@
+mysql> UPDATE etudiant SET age = 20 WHERE nom = "Spaghetti" AND prenom = "Betty";
+Query OK, 1 row affected (0.03 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> SELECT * FROM etudiant WHERE nom = "Spaghetti" AND prenom = "Betty";
++----+-----------+--------+-----+---------------------------------+
+| id | nom       | prenom | age | email                           |
++----+-----------+--------+-----+---------------------------------+
+|  1 | Spaghetti | Betty  |  20 | betty.Spaghetti@laplateforme.io |
++----+-----------+--------+-----+---------------------------------+
+1 row in set (0.02 sec)
+
+mysql> 
+
