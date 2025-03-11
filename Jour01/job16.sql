fcf8fbf1a8e5313227e5ae39f1e3f323da4cb951
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 03:05:47 +0100

    Retrieve all students whose first name begins with a “b”

---
+++ Jour01/job16.sql
@@ -0,0 +1,10 @@
+mysql> SELECT * FROM etudiant WHERE prenom LIKE "B%";
++----+-----------+--------+-----+---------------------------------+
+| id | nom       | prenom | age | email                           |
++----+-----------+--------+-----+---------------------------------+
+|  1 | Spaghetti | Betty  |  23 | betty.Spaghetti@laplateforme.io |
+|  4 | Barnes    | Binkie |  16 | binkie.barnes@laplateforme.io   |
++----+-----------+--------+-----+---------------------------------+
+2 rows in set (0.00 sec)
+
+mysql> 
