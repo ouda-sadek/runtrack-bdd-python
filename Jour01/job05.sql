897475d49351971b9a7cf0f82c74bb809355b821
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 01:44:58 +0100

    add students to table etudiant

---
+++ Jour01/job05.sql
@@ -0,0 +1,8 @@
+mysql> INSERT INTO etudiant (nom, prenom, age, email) VALUES
+    -> ("Spaghetti", "Betty", 23, "betty.Spaghetti@laplateforme.io"),
+    -> ("Steak", "Chuck", 45, "chuck.steak@laplateforme.io"),
+    -> ("Doe", "John", 18, "john.doe@laplateforme.io"),
+    -> ("Barnes", "Binkie", 16, "binkie.barnes@laplateforme.io"),
+    -> ("Dupuis", "Gertrude", 20, "gertrude.dupuis@laplateforme.io");
+Query OK, 5 rows affected (0.05 sec)
+Records: 5  Duplicates: 0  Warnings: 0
