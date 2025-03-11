dbc07cf3eb61a91731d9a252355538a8cb6cdffd
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-11 01:25:42 +0100

    create database LaPlateforme

---
+++ Jour01/job02.sql
@@ -0,0 +1,14 @@
+mysql> CREATE DATABASE LaPlateforme;
+Query OK, 1 row affected (0.05 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| LaPlateforme       |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+5 rows in set (0.06 sec)
